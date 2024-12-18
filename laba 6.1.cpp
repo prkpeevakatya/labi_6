cf33ba28970a3c4dd44d13d35ecbed003831c2e1
Katya <129635004+prkpeevakatya@users.noreply.github.com>
2024-12-18 06:27:15 +0200

    Update laba 6.1.cpp

--- laba 6.1.cpp
+++ laba 6.1.cpp
@@ -4,34 +4,7 @@
 #include "helpp.hpp"
 using namespace std;
 
-bool first_digit_one(int a)
-{
-    int last = a;
-    while (last > 10 )
-        last /= 10;
-    if (last == 1) return true;
-    return false;
-}
 
-int* create_and_fill_array(int maxi)
-{
-    int* A = new int[maxi];
-
-    for (int i = 0; i < maxi; i++)
-        cin >> A[i];
-
-    return A;
-}
-
-void sort_mass(int* A, int maxi)
-{
-    for (int i = 0; i < maxi; i++)
-        for (int j = i + 1; j < maxi; j++)
-        {
-            if (A[i] > A[j])
-                swap(A[i], A[j]);
-        }
-}
 
     int main() {
         setlocale(LC_ALL, "Russian");

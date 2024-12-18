9bfd89fcb8d54f6a363e17f45bff1633760fe37c
Katya <129635004+prkpeevakatya@users.noreply.github.com>
2024-12-18 07:52:21 +0200

    Update helpp.hpp

--- helpp.hpp
+++ helpp.hpp
@@ -5,3 +5,5 @@ int summ_of_digit(int a);
 int first_digit(int a);
 int** create_and_fill_array3(int n);
 int summ_string(int** Q, int n, int w);
+bool contains_7_not_6(int x);
+bool is_prime(int x);

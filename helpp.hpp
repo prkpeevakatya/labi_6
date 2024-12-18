b2b214d53aad66eb924d61cb8c8d0882c7715a8f
Katya <129635004+prkpeevakatya@users.noreply.github.com>
2024-12-18 07:20:53 +0200

    Update helpp.hpp

--- helpp.hpp
+++ helpp.hpp
@@ -3,3 +3,5 @@ int* create_and_fill_array(int maxi);
 bool first_digit_one(int a);
 int summ_of_digit(int a);
 int first_digit(int a);
+int** create_and_fill_array3(int n);
+int summ_string(int** Q, int n, int w);
